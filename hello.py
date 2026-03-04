abee9909dc3d6ba44966adc6d916ce8f83fcc6f6
flavio <flavio.ricci82@gmail.com>
2026-03-04 19:17:48 +0100

    fixed

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+import requests
+
+# Cerchiamo i dati di Pikachu
+pokemon = "pikachu"
+url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+
+try:
+    response = requests.get(url)
+    response.raise_for_status() # Controlla se ci sono errori HTTP
+    data = response.json()
+    
+    nome = data['name'].capitalize()
+    tipo = data['types'][0]['type']['name']
+    print(f"Nome: {nome} | Tipo principale: {tipo}")
+
+except Exception as e:
+    print(f"Si è verificato un errore: {e}")
